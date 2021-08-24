45f46e744af8f2dbabce1c4cb2f0eb4ef3ac7915
Soul-Remix <soulremix-webdev@pm.me>
2021-08-24 17:41:35 +0300

    create footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 import { Route } from 'react-router';
 import HomePage from './pages/HomePage';
 
@@ -7,6 +8,7 @@ const App = () => {
     <>
       <NavBar />
       <Route path="/" exact component={HomePage} />
+      <Footer />
     </>
   );
 };
